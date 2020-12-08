bbc470a46240ea8174a03f832a8d013d290a0836
mick-d97 <mickael.dolphin@live.fr>
2020-12-08 13:43:34 +0100

    ajout de l'exercice 3 partie 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$km = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 3 partie 1</title>
+</head>
+<body>
+   <p><?=$km?></p>
+   <?php
+$km = 3;
+?>
+<p><?=$km?></p>
+   <?php
+$km = 125;
+?>
+<p><?=$km?></p>
+</body>
+</html>
